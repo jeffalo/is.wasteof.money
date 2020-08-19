0809ba5e725d9d88d88f7fe1b9cf8a1c93c49c32
StudioChew <roope.aulis.oliver@gmail.com>
2020-08-19 08:38:29 +0300

    added linux and rickroll

--- items.js
+++ items.js
@@ -36,4 +36,6 @@ const items  = [
     {name: 'minecraft 3ds', isWaste: true},
     {name: 'drugs', isWaste: 'custom', custom: 'depends', explanation: 'im not qualified to tell this to you speak to your doctor or something'}, 
     {name: 'life', isWaste: 'custom', custom: 'depends', explanation: 'If you mean your life, totally, but life in general ... well, I guess, living stuff created money and also ways to waste it, so ... kinda ...'}
+    {name: 'linux', isWaste: 'free'}
+    {name: 'rickroll', isWaste: 'custom', custom: '<a href="https://wasteof.money/">click here to find out</a>'}
 ];
