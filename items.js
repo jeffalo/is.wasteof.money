dea91ad6060688dba357117ffde767e1bc36bdd5
MasterOfCards <64666021+MasimbaTheHuman@users.noreply.github.com>
2020-12-08 18:25:21 -0800

    Added Zoom, Zapier, Apple, and Kurzgesagt

--- items.js
+++ items.js
@@ -1,39 +1,4 @@
-const items  = [
-    {name: 'iphone', isWaste: true},
-    {name: 'android', isWaste: false},
-    {name: 'minecraft', isWaste: false},
-    {name: 'air', isWaste: 'free'},
-    {name: 'https', isWaste: 'custom', custom: 'depends', explanation: 'https is not a waste of money if you get it free'},
-    {name: 'this website', isWaste: 'custom', custom: 'how dare you check if this website is a waste of money!! its free how could it possibly be???'},
-    {name: 'optifine', isWaste: 'free'},
-    {name: 'optifine cape', isWaste: false},
-    {name: 'samsung', isWaste: true},
-    {name: 'google', isWaste: 'free'},
-    {name: 'minecraft bedrock', isWaste: true},
-    {name: 'fortnite', isWaste: 'free', explanation: 'but... this <i>is</i> fortnite soooo.... maybe thats not so true'},
-    {name: 'money', isWaste: true},
-    {name: 'netflix', isWaste: true},
-    {name: 'hulu', isWaste: true},
-    {name: 'youtube red', isWaste: true},
-    {name: 'humble bundle', isWaste: true},
-    {name: 'pizza', isWaste: 'custom', custom: 'never', explanation: 'unless it is a <a href="https://i.pinimg.com/originals/3a/be/70/3abe70be046eed475708ee9b0dd21179.jpg">golden pizza</a>'},
-    {name: 'testing', isWaste: true, explanation: 'testing is a waste of time and time is money.'},
-    {name: 'terraria', isWaste: false, explanation: 'its kinda like minecraft and minecraft is good so no not a waste'},
-    {name: 'faithful', isWaste: 'custom', custom: 'depends'},
-    {name: 'css', isWaste: 'true', explanation: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'},
-    {name: 'water', isWaste: 'custom', custom: 'depends', explanation: 'it really depends on a lot of things'},
-    {name: 'toilet paper', isWaste: true, explanation: 'welcome to 2020'},
-    {name: 'i', isWaste: true},
-    {name: 'my life', isWaste: true},
-    {name: 'pain', isWaste: 'custom', custom: 'depends', explanation: 'what kind of pain are you talking about? bread is not a waste of money'},
-    {name: 'infinite pain', isWaste: 'free', explanation: 'even though this is free i\'d say its still a waste of free'},
-    {name: 'video games', isWaste: 'custom', custom: 'depends', explanation: 'video games is too general i need more than just video games how am i supposed to tell you if all video games are a waste of money?? some are even free so i dont understand what is your problem???'},
-    {name: 'minecraft dungeons', isWaste: false},
-    {name: 'internet', isWaste: true},
-    {name: 'seus ptgi', isWaste: false},
-    {name: 'apple pro stand', isWaste: true, explanation: 'A MONITOR STAND FOR 999$?!? ARE YOU FOR REAL, IT DEFINITELY IS A WASTE OF MONEY REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE'},
-    {name: 'microtransactions in mobile games', isWaste: true},
-    {name: 'minecraft 3ds', isWaste: true},
-    {name: 'drugs', isWaste: 'custom', custom: 'depends', explanation: 'im not qualified to tell this to you speak to your doctor or something'}, 
-    {name: 'life', isWaste: 'custom', custom: 'depends', explanation: 'If you mean your life, totally, but life in general ... well, I guess, living stuff created money and also ways to waste it, so ... kinda ...'}
-];
+{name: 'Apple', isWaste: true}
+{name: 'zapier', isWaste: true}
+{name: 'Kurzgesagt', isWaste: false}
+{name: 'Zoom', isWaste: true}
