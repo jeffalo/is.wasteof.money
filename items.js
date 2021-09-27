339ea72021806213401acb9a02a558d9db525b7a
dash-gr-test <91324556+dash-gr-test@users.noreply.github.com>
2021-09-27 11:32:23 -0400

    Update items.js

--- items.js
+++ items.js
@@ -1,6 +1,6 @@
 const items  = [
     {name: 'ebay.com', isWaste: 'custom', custom: 'Online store where people can buy stuff new and used, online auctions and bidding on items. Users can easily create an account and start selling items.},
     {name: 'github.com', isWaste: 'custom', custom: 'Open-source website to create and share groups of code called repositories.'},
-    {name: 'amazon.com', isWaste: 'custom', custom: 'Huge online shopping company created by Jeff Bezos. '}
+    {name: 'amazon.com', isWaste: 'custom', custom: 'Huge online shopping company created by Jeff Bezos. '},
     {name: 'google.com', isWaste: 'custom', custom: 'Google is a large company which develops web-based applications, but are mostly well-known for their world-famous search engine called Google. Google also has Gmail, Google Chat, Google Meet, Google Drive, Docs, Sheets, Slides, Forms, Google Maps and many more internet services.'},
 ];
